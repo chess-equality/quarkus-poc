58a38fe81e4a13047ad00ab9b438dd3d575f9fee
chess-equality <chessequality@gmail.com>
2019-04-07 15:02:00 +0800

    - Changed Docker image name

--- src/test/java/org/chessequality/quarkus/poc/resource/NativeGreetingResourceIT.java
+++ src/test/java/org/chessequality/quarkus/poc/resource/NativeGreetingResourceIT.java
@@ -3,7 +3,7 @@ package org.chessequality.quarkus.poc.resource;
 import io.quarkus.test.junit.SubstrateTest;
 
 @SubstrateTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+class NativeGreetingResourceIT extends GreetingResourceTest {
 
     // Execute the same tests but in native mode.
 }
